d45978175fc6d023683f9154ddaabfcd9e39dbd3
Yousef Sawires <ysawires@clearpointneuro.com>
2023-03-10 14:38:30 -0500

    Creating User Control for ProductsCatalog. Refactoring out of MainWindow.xaml

---
+++ SuperMarket.MVC/ProductCatalogUI.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SuperMarket.MVC
+{
+    public partial class ProductCatalogUI : UserControl
+    {
+        public static readonly DependencyProperty ProductsProperty = DependencyProperty.Register(
+            nameof(Products), typeof(IEnumerable<Product>), typeof(ProductCatalogUI), new PropertyMetadata(Array.Empty<Product>()));
+
+        public IEnumerable<Product> Products
+        {
+            get { return (IEnumerable<Product>) GetValue(ProductsProperty); }
+            set { SetValue(ProductsProperty, value); }
+        }
+        
+        public ProductCatalogUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
